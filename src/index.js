c2af16c7b7f961624e4973fdc54fe7ab9bb4708f
juan.vergaram@mail.udp.cl <juan.vergaram@mail.udp.cl>
2023-10-16 00:39:28 -0300

    First commit with folder structure and env vars

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 8080
+
+
+app.listen(port, ()=>{
+    console.log(`Server started on port ${port}`);
+})
